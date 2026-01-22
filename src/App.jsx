6099059c49220f8a58409c8a040a18aa1c3c11f5
Pruthvi1717 <pruthviraj.g@somaiya.edu>
2026-01-22 17:09:06 +0530

    changed the settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-import Navigation from './Navigation/nav';
+import Navigation from './Navigation/ Nav';
 import Products from './Products/Products';
 import Recommended from './Recommended/Recommended';
 import Category from './Sidebar/Category/Category';
